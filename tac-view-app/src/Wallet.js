a40aba91d3dc59f6374fe58b41b340b1bff2a11f
jason <jsunardi6061@sdsu.edu>
2022-02-03 19:07:42 -0800

    first_commit

---
+++ tac-view-app/src/Wallet.js
@@ -0,0 +1,130 @@
+import {React, useState, useEffect} from 'react'
+import {ethers} from 'ethers'
+import styles from './style.module.css'
+import token_abi from './contracts/token_abi.json'
+
+
+const Wallet = () => {
+
+	let contractAddress = '0x9eC401A74553E8E27B8f5E5fd1A2829cfD0C6230';
+
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+	const [provider, setProvider] = useState(null);
+	const [signer, setSigner] = useState(null);
+	const [contract, setContract] = useState(null);
+
+	const [tokenName, setTokenName] = useState("Token");
+	const [balance, setBalance] = useState(null);
+
+
+
+	const connectWalletHandler = () => {
+		if (window.ethereum && window.ethereum.isMetaMask) {
+
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				accountChangedHandler(result[0]);
+				setConnButtonText('Wallet Connected');
+			})
+			.catch(error => {
+				setErrorMessage(error.message);
+			
+			});
+
+		} else {
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+
+	const accountChangedHandler = (newAccount) => {
+		setDefaultAccount(newAccount);
+		updateEthers();
+	}
+
+	const updateBalance = async () => {
+		let balanceBigN = await contract.balanceOf(defaultAccount);
+		let balanceNumber = balanceBigN.toNumber();
+
+		let tokenDecimals = await contract.decimals();
+
+		let tokenBalance = balanceNumber / Math.pow(10, tokenDecimals);
+
+		setBalance(toFixed(tokenBalance));	
+
+
+	}
+
+   function toFixed(x) {
+   if (Math.abs(x) < 1.0) {
+      var e = parseInt(x.toString().split('e-')[1]);
+      if (e) {
+         x *= Math.pow(10, e - 1);
+         x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+      }
+   } else {
+      var e = parseInt(x.toString().split('+')[1]);
+      if (e > 20) {
+         e -= 20;
+         x /= Math.pow(10, e);
+         x += (new Array(e + 1)).join('0');
+      }
+   }
+   return x;
+}
+
+	const chainChangedHandler = () => {
+		window.location.reload();
+	}
+
+
+	window.ethereum.on('accountsChanged', accountChangedHandler);
+
+	window.ethereum.on('chainChanged', chainChangedHandler);
+
+	const updateEthers = () => {
+		let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+		setProvider(tempProvider);
+
+		let tempSigner = tempProvider.getSigner();
+		setSigner(tempSigner);
+
+		let tempContract = new ethers.Contract(contractAddress, token_abi, tempSigner);
+		setContract(tempContract);	
+	}
+
+	useEffect(() => {
+		if (contract != null) {
+			updateBalance();
+			updateTokenName();
+		}
+	}, [contract]);
+
+	const updateTokenName = async () => {
+		setTokenName(await contract.name());
+	}
+	
+	return (
+	<div>
+			<h2> {tokenName + " ERC-20 Wallet"} </h2>
+			<button className={styles.button6} onClick={connectWalletHandler}>{connButtonText}</button>
+
+			<div className={styles.walletCard}>
+			<div>
+				<h3>Address: {defaultAccount}</h3>
+			</div>
+
+			<div>
+				<h3>{tokenName} Balance: {balance}</h3>
+			</div>
+
+			{errorMessage}
+		</div>
+	</div>
+	)
+}
+
+export default Wallet;
