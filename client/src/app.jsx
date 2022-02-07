5a0a4c594550c5ff8152f347a871f35a5cfa9d42
Taniel Pogharian <tanielpogharian@Taniels-MacBook-Pro.local>
2022-02-07 12:50:54 -0700

    Complete

---
+++ client/src/app.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+import axios from 'axios';
+
+const App = (props) => {
+
+  const [balance, setBalance] = useState(null);
+
+  const handleClick = () => {
+    axios.get('http://localhost:8080/getBalance')
+    .then((data) => {
+      const amount = data.data / 100000000;
+      setBalance(amount)
+    })
+  }
+
+  return (
+    <div className="app">
+      <div className="window">
+        <h1>Your balance is: {balance}</h1>
+        <button onClick={handleClick}>Get Balance</button>
+      </div>
+
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
